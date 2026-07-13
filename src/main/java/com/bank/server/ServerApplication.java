162d4a8339bf30ee7a0a359292fb2c065f122cfc
Ammar Hasan <ammar.hasan@osbgroup.co.uk>
2026-07-13 16:59:02 +0530

    started with jwt service

--- src/main/java/com/bank/server/ServerApplication.java
+++ src/main/java/com/bank/server/ServerApplication.java
@@ -1,5 +1,4 @@
 package com.bank.server;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,5 +8,4 @@ public class ServerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
 	}
-
 }
---
+++ src/main/java/com/bank/server/security/JwtService.java
@@ -0,0 +1,15 @@
+package com.bank.server.security;
+
+
+import lombok.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+    @Value("${security.jwt.secret-key}")
+    private String secretKey;
+
+    @Value("${security.jwt.expiration-time}")
+    private long jwtExpiration;
+
+}
