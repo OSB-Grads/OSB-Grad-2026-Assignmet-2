5acebcbd765b2e4a8149f7f4d236ae4bae2352e4
Ammar Hasan <ammar.hasan@osbgroup.co.uk>
2026-07-13 17:02:06 +0530

    Added RequestStatus in CustomerNotFoundException

--- src/main/java/com/bank/server/exception/CustomerNotFoundException.java
+++ src/main/java/com/bank/server/exception/CustomerNotFoundException.java
@@ -1,5 +1,8 @@
 package com.bank.server.exception;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class CustomerNotFoundException extends RuntimeException{
     public CustomerNotFoundException(String message) {
         super(message);
