a39127556d833157f58cede2bea3d9402f5ed8f7
Shivakumar Khot <shivakumar.khot@osbgroup.co.uk>
2026-07-14 17:10:19 +0530

    GEN-253/global exception

--- src/main/java/com/bank/server/advice/GlobalExceptionHandler.java
+++ src/main/java/com/bank/server/advice/GlobalExceptionHandler.java
@@ -1,18 +1,39 @@
 package com.bank.server.advice;
 
+import com.bank.server.dto.response.ErrorResponse;
+import com.bank.server.enums.LogType;
 import com.bank.server.exception.TransactionNotFoundException;
+import com.bank.server.service.LoggerService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
+@RequiredArgsConstructor
 public class GlobalExceptionHandler {
 
+    private final LoggerService loggerService;
+
     @ExceptionHandler(TransactionNotFoundException.class)
-    public ResponseEntity<String> handleTransactionNotFound(
-            TransactionNotFoundException ex) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body(ex.getMessage());
+    public ResponseEntity<ErrorResponse> handleTransactionNotFound(TransactionNotFoundException ex) {
+
+        log.warn("{}", ex.getMessage());
+
+        loggerService.log(
+                ex.getCode(),
+                ex.getMessage(),
+                LogType.ERROR
+        );
+
+        ErrorResponse error = ErrorResponse.builder()
+                .code(ex.getCode())
+                .message(ex.getMessage())
+                .build();
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
     }
 }
