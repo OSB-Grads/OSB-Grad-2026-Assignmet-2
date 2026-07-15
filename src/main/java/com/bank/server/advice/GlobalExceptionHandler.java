b05f2cbfadcad6182cf3a7333ad35a2bc69bc03c
Akash Shankrayya <akash.shankrayya@osbgroup.co.uk>
2026-07-15 12:02:31 +0530

    #GEN-271 - Changes according to endpoints

--- src/main/java/com/bank/server/advice/GlobalExceptionHandler.java
+++ src/main/java/com/bank/server/advice/GlobalExceptionHandler.java
@@ -1,18 +1,36 @@
 package com.bank.server.advice;
 
+import com.bank.server.enums.LogType;
+import com.bank.server.exception.AccountNotFoundException;
 import com.bank.server.exception.TransactionNotFoundException;
+import com.bank.server.exception.UserNotFoundException;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(TransactionNotFoundException.class)
-    public ResponseEntity<String> handleTransactionNotFound(
-            TransactionNotFoundException ex) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body(ex.getMessage());
+    @ExceptionHandler(AccountNotFoundException.class)
+    public ResponseEntity<String> handleAccountNotFound(
+            AccountNotFoundException ex) {
+
+        log.warn("{}", ex.getMessage());
+
+        loggerService.log(
+                ex.getCode(),
+                ex.getMessage(),
+                LogType.ERROR);
+
+        ErrorResponse error = ErrorResponse.builder()
+                .code(ex.getCode())
+                .message(ex.getMessage())
+                .build();
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
     }
+
 }
