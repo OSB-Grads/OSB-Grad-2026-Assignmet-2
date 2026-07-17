c4a4f7cac4b33518037a988c8a3befbe11449995
ammarhasan6679 <123263921+ammarhasan6679@users.noreply.github.com>
2026-07-17 15:47:54 +0530

    Gen 259 (#15)
    * Updated pom.xml
    
    * #259 - Implemented JwtService
    
    * Refactored JwtService code
    
    * Changed long to Long in JWTService
    
    * 259 - Implemeted extractCustoemrId from token method in JWTService
    
    * Changed subject from userName to customerId
    
    * 252 - Changed Jwt service and AuthController AuthService
    
    * 259 Added null check for contextHolder in JwtFilters
    
    ---------
    
    Co-authored-by: Ammar Hasan <ammar.hasan@osbgroup.co.uk>

---
+++ src/main/java/com/bank/server/security/CustomUserDetails.java
@@ -0,0 +1,57 @@
+package com.bank.server.security;
+
+import com.bank.server.entity.Auth;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@RequiredArgsConstructor
+public class CustomUserDetails implements UserDetails {
+
+    private final Auth auth;
+
+    public String getCustomerId() {
+        return auth.getId();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(
+                new SimpleGrantedAuthority("ROLE_" + auth.getRole().name())
+        );
+    }
+
+    @Override
+    public String getPassword() {
+        return auth.getPasswordHash();
+    }
+
+    @Override
+    public String getUsername() {
+        return auth.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
