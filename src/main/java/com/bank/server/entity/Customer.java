09f9c77fc26806c515b290fca4354d9dd815603e
CharuBohra <charubohra2004@gmail.com>
2026-07-22 23:20:09 +0530

    Added JUnit cases for PaymentService and PaymentProcessorService

--- src/main/java/com/bank/server/entity/Customer.java
+++ src/main/java/com/bank/server/entity/Customer.java
@@ -2,6 +2,7 @@ package com.bank.server.entity;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -9,6 +10,7 @@ import java.time.LocalDateTime;
 
 @Entity
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class Customer {
